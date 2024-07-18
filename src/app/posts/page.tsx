00c2ca570265ebefe6d80ab9a95c36ebe24cdbb2
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-07-18 14:58:34 +0200

    Listing all Posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
-    return (
-        <main className="text-center pt-32 px-5">
-        <h1 className="text-4xl sm:text-5xl font-bold mb-5">All Posts</h1>
-      </main>
-    );
-  }
-  
+import PostsList from '@/components/posts-list';
+import { Suspense } from 'react';
+
+export default async function Home() {
+  return (
+    <main className="text-center pt-16 px-5">
+      <h1 className="text-4xl sm:text-5xl font-bold mb-5">All Posts</h1>
+      <Suspense fallback="Loading...">
+        <PostsList />
+      </Suspense>
+    </main>
+  );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,6 +13,10 @@ const navLinks = [
     href: '/posts',
     label: 'Posts',
   },
+  {
+    href: '/create-post',
+    label: 'Create posts',
+  },
 ];
 
 export default function Header() {
